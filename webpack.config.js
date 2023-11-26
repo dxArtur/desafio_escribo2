32cee5bb663a5c0b5b7ed1bff31b66d9648d9f0b
dxArtur <daniel.arturjsb@gmail.com>
2023-11-26 09:14:40 -0300

    output path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.join(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'public'),
 	},
 
 	module: {
